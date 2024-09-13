14263be07e7cbe91d54fa61ff22559bc61c48ef9
mka <mka@A34PC10>
2024-09-13 14:29:16 +0300

    task1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("-------середня оцінка------");
+
+Console.Write("enter your grade 1: ");
+double mark1 = double.Parse(Console.ReadLine());
+
+Console.Write("enter your grade 2: ");
+double mark2 = double.Parse(Console.ReadLine());
+
+Console.Write("enter your grade 3: ");
+double mark3 = double.Parse(Console.ReadLine());
+
+Console.Write("enter your grade 4: ");
+double mark4 = double.Parse(Console.ReadLine());
+
+Console.Write("enter your grade 5: ");
+double mark5 = double.Parse(Console.ReadLine());
+
+double result = (mark1 + mark2 + mark3 + mark4 + mark5) / 5;
+
+Console.Write(result);
