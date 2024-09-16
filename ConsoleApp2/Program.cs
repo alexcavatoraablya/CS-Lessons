f5cec551c811e973455a55a2a982950e57c99aaf
mka <mka@A34PC10>
2024-09-16 13:59:56 +0300

    завдання 1 (градус в фаренгейт)

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,20 +1,21 @@
-﻿Console.WriteLine("-------середня оцінка------");
+﻿Console.WriteLine("-------кількість градусів на вулиці------");
 
-Console.Write("enter your grade 1: ");
-double mark1 = double.Parse(Console.ReadLine());
+Console.Write("enter degrees: ");
+double degrees = double.Parse(Console.ReadLine());
 
-Console.Write("enter your grade 2: ");
-double mark2 = double.Parse(Console.ReadLine());
+//Console.Write("enter your grade 2: ");
+//double mark2 = double.Parse(Console.ReadLine());
 
-Console.Write("enter your grade 3: ");
-double mark3 = double.Parse(Console.ReadLine());
+//Console.Write("enter your grade 3: ");
+//double mark3 = double.Parse(Console.ReadLine());
 
-Console.Write("enter your grade 4: ");
-double mark4 = double.Parse(Console.ReadLine());
+//Console.Write("enter your grade 4: ");
+//double mark4 = double.Parse(Console.ReadLine());
 
-Console.Write("enter your grade 5: ");
-double mark5 = double.Parse(Console.ReadLine());
+//Console.Write("enter your grade 5: ");
+//double mark5 = double.Parse(Console.ReadLine());
 
-double result = (mark1 + mark2 + mark3 + mark4 + mark5) / 5;
+double result = (degrees * 1.8 + 32);
 
-Console.Write(result);
+Console.Write($"Fahrenheit:{result}");
+
